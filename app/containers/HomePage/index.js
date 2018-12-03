269d4864884bc82397c6cc270db1dc5241ff84cb
Alex Rozgo <alex.rozgo@gmail.com>
2018-12-02 23:06:25 -0600

    simple test

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -17,9 +17,7 @@ import messages from './messages';
 export default class HomePage extends React.PureComponent {
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <img src={require('../../images/rps.png')} width="100%" height="100%"></img>
     );
   }
 }
